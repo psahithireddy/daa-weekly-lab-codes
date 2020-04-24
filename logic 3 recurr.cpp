eb0e207fd2011699e1ebe679d547d213c75115e7
psahithireddy <62285383+psahithireddy@users.noreply.github.com>
2020-04-24 08:45:17 +0530

    Add files via upload

---
+++ logic 3 recurr.cpp
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<stdlib.h>
+int gcd(int a,int b)
+{
+if(a==0)
+ {
+ return b;}
+ gcd(b%a,a);
+ 
+}
+int main()
+{int t,a[100],i,z,u;
+printf("enter value of k\n");
+scanf("%d",&t);
+for(i=0;i<t;i++)
+{
+  scanf("%d",&a[i]);	
+  if(a[i]<0)
+  a[i]=-1*a[i];
+}
+for(i=0;i<t;i++)
+{
+  if(a[i]==0)
+  u=u+1;
+}
+ if(u==t)
+  printf("gcd doesnt exist\n");
+ else
+  {
+    for(i=0;i<t-1;i++)
+    {
+      z=gcd(a[i],a[i+1]);
+      a[i+1]=z;
+    }
+  }  
+printf("gcd is"); 			
+return z;
+}
